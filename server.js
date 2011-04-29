0ad8ce0dfa8ae774bca2a4ee58620a2eb133172c
Ruben Verborgh <ruben.verborgh@gmail.com>
2011-04-29 15:00:05 +0200

    Always return a status code.

--- server.js
+++ server.js
@@ -55,14 +55,13 @@ function getPhoto(req, res, next) {
       (accept.indexOf('*/*') !== -1)) {
     var location = 'http://' + host + ':' + port + '/restdesc/photos/' + id +
         '/faces';
-    res.header('Content-Type', 'image/jpg');
-    res.header('Link', '<' + location +
-        '>; rel="describedby"; title="contained faces"; type="text/n3"');
-    fs.readFile(__dirname + '/photos/' + id + '.jpg', function (err, data) {
-      if (err) throw err;
-      res.send(data);
-    });      
+    var fileName = __dirname + '/photos/' + id + '.jpg';
+    respondWithFile(res, fileName, 'image/jpg', {
+      'Link': '<' + location + '>; rel="describedby"; title="contained faces"; type="text/n3"'
+    });
   }
+  else
+    res.send('', 406);
 }
 
 function postPhoto(req, res, next) {
@@ -70,7 +69,7 @@ function postPhoto(req, res, next) {
   form.parse(req, function(err, fields, files) {    
     //res.write('received upload:\n\n');
     //res.end(sys.inspect({fields: fields, files: files}));
-    var file = files.photo.name;
+    var file = files.photo && files.photo.name;
     var id;
     if (file) {
       if (file === 'obama-gillard.jpg') {
@@ -79,6 +78,8 @@ function postPhoto(req, res, next) {
         id = 2;
       } else if (file === 'obama-rousseff.jpg') {
         id = 3;
+      } else {
+        return res.send('', 412);
       }
       console.log('Photo ID ' + id);
       var location = 'http://' + host + ':' + port + '/restdesc/photos/' + id;
@@ -99,43 +100,40 @@ function getFaces(req, res, next) {
   var id = pathname.replace(path, '$1');
   var accept = req.header('Accept', '*/*');
   if ((accept.indexOf('text/n3') !== -1) ||
-      (accept.indexOf('*/*') !== -1)) {  
-    fs.readFile(__dirname + '/photos/' + id + '.n3', function (err, data) {
-      if (err) throw err;
-      res.header('Content-Type', 'text/n3');
-      var relatedCount = {'1': 2, '2': 4, '3': 6}[id];
-      var linkHeaders = '';
-      for(var faceId = 1; faceId <= relatedCount; faceId++) {
-        var location1 = 'http://' + host + ':' + port + '/restdesc/photos/' +
-            id + '/faces/' + faceId;
-        var personId = faceId;    
-        var location2 = 'http://' + host + ':' + port + '/restdesc/photos/' +
-            id + '/persons/' + personId;
+      (accept.indexOf('*/*') !== -1)) {
+    var relatedCount = {'1': 2, '2': 4, '3': 6}[id] || 0;
+    var linkHeaders = '';
+    for(var faceId = 1; faceId <= relatedCount; faceId++) {
+      var location1 = 'http://' + host + ':' + port + '/restdesc/photos/' +
+          id + '/faces/' + faceId;
+      var personId = faceId;
+      var location2 = 'http://' + host + ':' + port + '/restdesc/photos/' +
+          id + '/persons/' + personId;
 
-        linkHeaders += (faceId > 1 ? ',\n      ' : '') + '<' + location1 +
-            '>; rel="related"; title="contained face"; type="image/jpg"' +
-            ',\n      <' + location2 +
-            '>; rel="related"; title="contained face"; type="text/plain"';        
-      }
-      res.header('Link', linkHeaders);
-      res.send(data);
-    });      
+      linkHeaders += (faceId > 1 ? ',\n      ' : '') + '<' + location1 +
+          '>; rel="related"; title="contained face"; type="image/jpg"' +
+          ',\n      <' + location2 +
+          '>; rel="related"; title="contained face"; type="text/plain"';
+    }
+    var fileName= __dirname + '/photos/' + id + '.n3';
+    respondWithFile(res, fileName, 'image/jpg', {
+      'Link': linkHeaders
+    });
   }
+  else
+    res.send('', 406);
 }
 
 function getFace(req, res, next) {
   var path = /^\/restdesc\/photos\/(\d+)\/faces\/(\d+)$/;
   var pathname = require('url').parse(req.url).pathname;
+  var fileName = __dirname + pathname.replace(path, '/photos/$1_$2.jpg');
   var accept = req.header('Accept', '*/*');
   if ((accept.indexOf('image/jpg') !== -1) ||
-      (accept.indexOf('*/*') !== -1)) {  
-    var fileName = __dirname + pathname.replace(path, '/photos/$1_$2.jpg');
-    fs.readFile(fileName, function (err, data) {
-      if (err) throw err;
-      res.header('Content-Type', 'image/jpg');
-      res.send(data);
-    });      
-  }
+      (accept.indexOf('*/*') !== -1))
+    respondWithFile(res, fileName, 'image/jpg');
+  else
+    res.send('', 406);
 }
 
 function getPerson(req, res, next) {
@@ -143,21 +141,32 @@ function getPerson(req, res, next) {
   var pathname = require('url').parse(req.url).pathname;
   var fileName = __dirname + pathname.replace(path, '/photos/$1_$2');
   var accept = req.header('Accept', '*/*');
-  if((accept.indexOf('text/n3') !== -1)) {
-      fs.readFile(fileName + '.n3', function (err, data) {
-        if (err) throw err;
-        res.header('Content-Type', 'text/n3');
-        res.send(data);
-      });
-  }
+  if((accept.indexOf('text/n3') !== -1))
+    respondWithFile(res, fileName + '.n3', 'text/n3');
   else if ((accept.indexOf('text/plain') !== -1) ||
-      (accept.indexOf('*/*') !== -1)) {
-    fs.readFile(fileName + '.txt', function (err, data) {
-      if (err) throw err;
-      res.header('Content-Type', 'text/plain');
-      res.send(data);
-    });      
-  }
+           (accept.indexOf('*/*') !== -1))
+    respondWithFile(res, fileName + '.txt', 'text/plain');
+  else
+    res.send('', 406);
+}
+
+function respondWithFile(res, fileName, contentType, headers) {
+  fs.readFile(fileName, function (err, data) {
+    if (err) {
+      if(err.code == 'ENOENT') {
+        res.send('', 404);
+        return false;
+      }
+      res.send(500);
+      throw err;
+    }
+    res.header('Content-Type', contentType);
+    if(headers)
+      for(name in headers)
+        res.header(name, headers[name]);
+    res.send(data);
+    return true;
+  });
 }
 
 app.listen(port);
